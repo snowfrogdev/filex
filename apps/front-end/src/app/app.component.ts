68b831942418b32d64255fe9eb0a830a5fe0acd5
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2021-10-01 21:31:17 -0400

    Capture dirs query param on angular app load

--- apps/front-end/src/app/app.component.ts
+++ apps/front-end/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Message } from '@file-explorer/api-interfaces';
+import { skip } from 'rxjs/operators';
 
 @Component({
   selector: 'file-explorer-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.pipe(skip(1)).subscribe((params) => {
+      console.log(params);
+    });
+  }
 }
--- apps/front-end/src/app/app.module.ts
+++ apps/front-end/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent],
 })
